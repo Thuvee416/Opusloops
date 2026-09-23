327d7717c1ef1b6eef4f8504cd357c63deada012
Thuvee Elankeeran <thuveem4@Thuvee16M4.local>
2026-09-23 14:33:44 -0400

    Remove Entirely online from landing headline

--- mobile/service-worker.js
+++ mobile/service-worker.js
@@ -1,6 +1,6 @@
 const CACHE_PREFIX = "opusloops-pwa-";
 const RETIRED_CACHE_PREFIXES = ["opusloops-mobile-"];
-const CACHE_NAME = `${CACHE_PREFIX}v60`;
+const CACHE_NAME = `${CACHE_PREFIX}v61`;
 const APP_SHELL = [
   "./",
   "./index.html",
